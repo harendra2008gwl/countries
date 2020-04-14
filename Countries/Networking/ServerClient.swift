992d06472ba9e586f6cfdba9600657301429a9a4
Harendra Sharma <harendra.sharma@techaheadcorp.com>
2020-04-14 23:03:16 +0530

    server connection class setup done for get request only.

--- Countries/Networking/ServerClient.swift
+++ Countries/Networking/ServerClient.swift
@@ -19,12 +19,48 @@ enum endPoint {
 }
 
 class ServerClient: NSObject {
-    //Globle access point
+    // Globle access point
     static let shared: ServerClient = {
         ServerClient()
     }()
+
     private let baseURL = "https://restcountries.eu/rest/v2/"
-    
-    
-    
+
+    func request(_ endpoint: endPoint, pharse: String, completetionHandler: @escaping (_ data: Data?, _ error: Error?) -> Void) {
+        guard let url = URL(string: "\(baseURL)\(endpoint.value)\(pharse)") else {
+            print("something went wrong")
+            return
+        }
+
+        print("request URL::: \(url)")
+
+        // create the session object
+        let session = URLSession.shared
+
+        // now create the URLRequest object using the url object
+        let request = URLRequest(url: url)
+
+        // create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, _, error in
+
+            guard error == nil else {
+                return
+            }
+
+            guard let data = data else {
+                return
+            }
+
+            do {
+                // create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+
+        task.resume()
+    }
 }
